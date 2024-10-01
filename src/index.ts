05e7c80e8ef8d76d53e94757c02023a1b45a4e6f
Ronan MOREL <ronan.morel@slickteam.fr>
2024-10-01 14:11:58 +0200

    fix: export interfaces from warp10 deps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { DataPoint, FormattedGTS, TimeUnits, GTS, W10Data } from '@senx/warp10';
 import { Warp10Service, DataPointResult, BuckertizerWarp10Enum, MapperWarp10Enum, ReducerWarp10Enum } from './warp10.service';
 
 @Module({
@@ -7,4 +8,17 @@ import { Warp10Service, DataPointResult, BuckertizerWarp10Enum, MapperWarp10Enum
 })
 class Warp10Module {}
 
-export { Warp10Module, Warp10Service, DataPointResult, BuckertizerWarp10Enum, MapperWarp10Enum, ReducerWarp10Enum };
+export {
+  Warp10Module,
+  Warp10Service,
+  DataPointResult,
+  BuckertizerWarp10Enum,
+  MapperWarp10Enum,
+  ReducerWarp10Enum,
+  // Warp10 class
+  DataPoint,
+  FormattedGTS,
+  TimeUnits,
+  GTS,
+  W10Data,
+};
